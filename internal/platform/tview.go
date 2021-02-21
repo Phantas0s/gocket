ef9e126e4813b27b8be40d4930ccc573d3a51519
matthieu <matthieu.cneude@gmail.com>
2021-02-21 22:13:39 +0100

    Mess around with TUI

--- internal/platform/tview.go
+++ internal/platform/tview.go
@@ -18,7 +18,23 @@ func (v *Tview) List(urls []string, titles []string) {
 	list.SetSelectedTextColor(tcell.ColorRed)
 	list.SetSelectedFocusOnly(true)
 
-	mapping(app)
+	pages := tview.NewPages()
+	modal := tview.NewModal().SetText("Hello").SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+		pages.HidePage("modal")
+	})
+
+	pages.AddPage("modal", modal, false, false)
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 'H' {
+			pages.ShowPage("modal")
+		}
+		return event
+	})
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		return event
+	})
 
 	for i, v := range titles {
 		url := urls[i]
@@ -28,31 +44,50 @@ func (v *Tview) List(urls []string, titles []string) {
 
 	}
 
+	list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 'j' {
+			list.SetCurrentItem(list.GetCurrentItem() + 1)
+		} else if event.Rune() == 'k' {
+			list.SetCurrentItem(list.GetCurrentItem() - 1)
+		}
+
+		return event
+	})
+
 	list.AddItem("Quit", "Press to exit", 'q', func() {
 		app.Stop()
 	})
 
-	if err := app.SetRoot(list, true).SetFocus(list).Run(); err != nil {
+	showpage := false
+	modal.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == '?' {
+			showpage = !showpage
+			pages.HidePage("modal")
+		}
+		return event
+	})
+
+	// Create the main layout.
+	layout := tview.NewFlex().
+		AddItem(list, 0, 1, false).
+		AddItem(pages, 0, 0, false)
+
+	layout.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == '?' {
+			showpage = !showpage
+			if showpage {
+				pages.ShowPage("modal")
+			}
+		}
+		return event
+	})
+
+	if err := app.SetRoot(layout, true).SetFocus(list).Run(); err != nil {
 		panic(err)
 	}
 }
 
 func help(app *tview.Application) {
-	app.NewModal().SetText("Help")
-}
-
-func mapping(app *tview.Application) {
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Rune() == 'x' {
-			help(app)
-		} else if event.Rune() == '?' {
-		}
-		return event
-	})
-
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		return event
-	})
 }
 
 // open opens the specified URL in the default browser of the user.
