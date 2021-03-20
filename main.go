b2172ad463ac7e2db561f41753000bc1c8e9e229
matthieu <matthieu.cneude@gmail.com>
2021-03-20 20:23:25 +0100

    Delete useless code

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"path/filepath"
-
 	"github.com/Phantas0s/gocket/cmd"
-	"github.com/adrg/xdg"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	initConfig()
 	cmd.Execute()
-}
-
-func initConfig() {
-	viper.AddConfigPath(filepath.Join(xdg.ConfigHome, "gocket"))
-	viper.AddConfigPath(".")
-	viper.SetConfigName("config")
-
-	viper.AutomaticEnv()
-	viper.ReadInConfig()
 }
